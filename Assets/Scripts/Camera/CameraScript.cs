cffd11ba03d364d150146c0a06232c0b43a204dd
AnuragNegii <negianurag99@gmail.com>
2023-05-20 11:11:55 +0530

    Fixed code
    fixed code for movement and camera.

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -4,20 +4,13 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
     private void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        transform.Rotate(30, 0, 0);
+        
+        //transform.Rotate(30, 0, 0);
     }
 
     private void LateUpdate()
     {
-        Vector3 plaeyrPos = player.transform.position;
-        
-
-        transform.position = plaeyrPos + new Vector3(0f, 7f, -8.0f);
-        
-
     }
 }
--- Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 10.0f;
+    [SerializeField] private float movementSpeed = 10.0f;
+    [SerializeField] private float rotationSpeed = 50.0f;
+    [SerializeField] private float jumpForce = 100.0f;
+
+    private float verticalInput;
+    private float horizontalInput;
+    private GameObject mainCamera;
+
     Rigidbody rb;
-    void Start()
+    private void Awake()
     {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
         rb = GetComponent<Rigidbody>();
     }
+    void Start()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
 
+    private void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        float verticalInput = Input.GetAxis("Vertical");
-        float horizontalInput = Input.GetAxis("Horizontal");
 
-        rb.AddForce(Vector3.forward * verticalInput * movementSpeed );
+        rb.AddRelativeForce(Vector3.forward * verticalInput * movementSpeed );
+        transform.Rotate(Vector3.up, rotationSpeed * horizontalInput * Time.deltaTime);
     }
 }
