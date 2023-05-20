10793dec5cabdcf3ba8f2c6499b88cb4e74d31b4
AnuragNegii <negianurag99@gmail.com>
2023-05-20 11:22:04 +0530

    jump added
    added jump functionality

--- Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -10,31 +10,25 @@ public class PlayerMovement : MonoBehaviour
 
     private float verticalInput;
     private float horizontalInput;
-    private GameObject mainCamera;
 
     Rigidbody rb;
     private void Awake()
     {
-        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
         rb = GetComponent<Rigidbody>();
     }
-    void Start()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-    }
-
     private void Update()
     {
-        verticalInput = Input.GetAxis("Vertical");
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        Debug.Log("spacePressed");
+    }
+    verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-
         rb.AddRelativeForce(Vector3.forward * verticalInput * movementSpeed );
         transform.Rotate(Vector3.up, rotationSpeed * horizontalInput * Time.deltaTime);
     }
